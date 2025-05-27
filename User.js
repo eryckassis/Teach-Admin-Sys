1b1940032372998d25e87c3e7a48c736b2ac78ca
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-26 21:16:21 -0300

    feat: implement User class with constructor and method for displaying user info

---
+++ User.js
@@ -0,0 +1,27 @@
+class User {
+  constructor(name, email, birthDate, role, active = true) {
+    this.name = name;
+    this.email = email;
+    this.birthDate = birthDate;
+    this.role = role || "estudante";
+    this.active = active;
+  }
+
+  exibirInfos() {
+    return `${this.name} - ${this.email}`;
+  }
+}
+
+const newUser = new User(
+  "baiano",
+  "baianagens@gmail.com ",
+  " 19/01/1990",
+  "estudante"
+);
+console.log({
+  isPrototype: User.prototype.isPrototypeOf(newUser),
+  userInfo: newUser.exibirInfos(),
+});
+
+// utlizado Objeto consolidado para informaçoes de objeto,
+// e assim eliminando a necessidade de criar varios console.log
