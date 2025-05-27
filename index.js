c1d0ba880c0cac03f4113b8173df326ccef68d0f
Eryck Assis <eng.assis.dev@gmail.com>
2025-05-26 22:52:46 -0300

    feat: update Docente and Admin classes to use export default and modify package.json for project configuration

---
+++ index.js
@@ -0,0 +1,6 @@
+import User from "./User.js";
+import Docente from "./Docente.js";
+import Admin from "./Admin.js";
+
+const novoUser = new User("Juliana", " j@j.com", "2024-01-01");
+console.log(novoUser.exibirInfos());
