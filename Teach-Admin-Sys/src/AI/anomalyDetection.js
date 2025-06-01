82b563905ae4ff3549d1155f573294593afb7620
Eryck Assis <eng.assis.dev@gmail.com>
2025-06-01 17:24:38 -0300

    chore: update documentation comments for anomaly detection module

--- Teach-Admin-Sys/src/AI/anomalyDetection.js
+++ Teach-Admin-Sys/src/AI/anomalyDetection.js
@@ -1,3 +1,16 @@
+/**
+ * @file anomalyDetection.js
+ * @description Detecta anomalias nos logs do usuário com base em uma média e um limite de sensibilidade.
+ * @module anomalyDetection
+ */
+
+/**
+ * Detecta anomalias nos logs de acesso do usuário.
+ * @param {Array<number} userLogs - lista de logs de acesso do usuário.
+ * @param {number} sensitivity - Sensibilidade para o limite de anomalia (1.0 = 100% da média).
+ * @returns {Boolean} -  Retorna true se uma anomalia for detectada, caso o contrario, retorna false.
+ */
+
 export function detectionAnomaly(userLogs, sensitivity = 1.5) {
   if (!Array.isArray(userLogs) || userLogs.length === 0) {
     console.error("Os logs do usuário devem ser um array não vazio.");
